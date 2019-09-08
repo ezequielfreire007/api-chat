85f50716a993ffdbd3d61293d3ad14244d141609
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2019-09-08 01:52:05 -0300

    add router

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const router = express.Router();
 
 var app = express();
 
-app.use('/', function(req, res){
-    res.send('Hola')
+// Router
+app.use(router)
+
+router.get('/message', function(req, res){
+    res.send('Lista e mensajes')
 })
+
+router.post('/message', function(req, res){
+    res.send('Mensaje agregado')
+})
+
+// app.use('/', function(req, res){
+//     res.send('Hola')
+// })
 
 app.listen(3000)
 
