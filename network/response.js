53fadf54aaca827be31ba83a544ea42e24b639c2
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2021-01-24 23:08:05 -0300

    add chalk for console.log

--- network/response.js
+++ network/response.js
@@ -1,3 +1,5 @@
+const chalk = require('chalk') // proporciona un color al console log
+
 exports.success = function (req, res, message, status){
     res.status(status || 200).send({ 
         'error': '', 
@@ -5,7 +7,8 @@ exports.success = function (req, res, message, status){
     })
 }
 
-exports.error = function (req, res, message, status){
+exports.error = function (req, res, message, status, detail){
+    console.error(chalk.red( `[response error] ${detail}` ))
     res.status(status || 500).send({ 
         'error': message, 
         'body': ''
