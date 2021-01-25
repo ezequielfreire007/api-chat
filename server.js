06b3751163f9dac36cfb3d2c8024eeb1d59cc904
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2021-01-24 23:09:27 -0300

    added chages in server

--- server.js
+++ server.js
@@ -2,42 +2,17 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 
-const response = require('./network/response')
-
-const router = express.Router();
-
+// const response = require('./network/response')
+const router = require('./network/routes')
 // Server express
 var app = express();
-
 // BodyParse
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extends: false }))
 
 // Router
-app.use(router)
-
-router.get('/message', function(req, res){
-    console.log(req.headers)
-    res.header({
-        "custom-header": "new header"
-    })
-    response.success(req, res, 'list messages', 201)
-})
-
-router.post('/message', function(req, res){
-    // res.send('Mensaje agregado')
-    response.success(req, res, 'create message', 201)
-})
-
-router.delete('/message', function(req, res){
-    console.log(req.query) // por url
-    if(req.query.error == 'ok'){
-        response.error(req, res, 'error simuldao', 400)
-    } else{
-        response.success(req, res, 'create message', 201)
-    }
-    response.success(req, res, 'delete message')
-})
+// app.use(router)
+router(app)
 
 // Archivos estaticos
 app.use('/app', express.static('public'))
